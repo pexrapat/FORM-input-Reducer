613602b0f4a059bb49c3242534e4f3eeb707fe78
368 ภีรภัทร์ <138644393+pexrapat@users.noreply.github.com>
2024-09-27 15:05:28 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+import form from '@tailwindcss/forms'
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [form],
+}
+
